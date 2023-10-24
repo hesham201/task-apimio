41b15a266ab44ef0698e8384d175180a1ae1b320
hesham112 <107617341+hesham201@users.noreply.github.com>
2023-10-24 19:39:40 +0500

    vercel config

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const router = express.Router;
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
+  console.log("listeing");
   res.sendFile(path.join(__dirname, "views", "home.html"));
 });
-app.listen(3000);
+app.listen(9000);
